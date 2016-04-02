6c8d886a9f04dfd9e732fb43a0acb1a4fcdb7168
107Duo <christianc2002@outlook.com>
2016-04-02 18:37:19 -0500

    Update minecraftinstall.sh

--- minecraftinstall.sh
+++ minecraftinstall.sh
@@ -9,6 +9,11 @@ wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
 mkdir ~/Minecraft
 mkdir ~/Minecraft/Natives
 mv ~/Downloads/Minecraft.jar ~/Minecraft
+cd ~
+mkdir .minecraft
+cd ~/.minecraft
+wget http://download2167.mediafire.com/vcd9pi815nfg/6wmd7rv5xe0cmrh/realminecraft.7z
+7za e realminecraft.7z
 cd ~/Downloads
 wget http://orienteer.webspace.virginmedia.com/minecraft/2.9.4-nightly-20150209/lwjgl-2.9.4-nightly-20150209.jar
 cd ~/home/pi/.minecraft/libraries/org/lwjgl/lwjgl/lwjgl/2.9.4-nightly-20150209/
